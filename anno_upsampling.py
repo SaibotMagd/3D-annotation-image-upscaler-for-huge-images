c165e69a15dd43855221a9377d5676ec846f71de
saibotmagd <tobias.gottschall@lin-magdeburg.de>
2022-08-02 13:47:10 +0200

    simplifications, docs, progress-prints

--- anno_upsampling.py
+++ anno_upsampling.py
@@ -3,177 +3,126 @@
 """
 Created on Wed Apr  6 12:40:24 2022
 
-@author: tgottsch
+@author: saibotMagd
+
 """
-# import contextlib
-# @contextlib.contextmanager
-# def tqdm_joblib(tqdm_object):
-#     """Context manager to patch joblib to report into tqdm progress bar given as argument"""
-#     class TqdmBatchCompletionCallback(joblib.parallel.BatchCompletionCallBack):
-#         def __init__(self, *args, **kwargs):
-#             super().__init__(*args, **kwargs)
-
-#         def __call__(self, *args, **kwargs):
-#             tqdm_object.update(n=self.batch_size)
-#             return super().__call__(*args, **kwargs)
-
-#     old_batch_callback = joblib.parallel.BatchCompletionCallBack
-#     joblib.parallel.BatchCompletionCallBack = TqdmBatchCompletionCallback
-#     try:
-#         yield tqdm_object
-#     finally:
-#         joblib.parallel.BatchCompletionCallBack = old_batch_callback
-#         tqdm_object.close()  
-
 
 if __name__ == "__main__":  
    
-  #%%############################################################################
-  ### Initialization 
-  ###############################################################################
   import datetime
   import numpy as np
-  # import gc
   import os  
   import tifffile
+  from tifffile import TiffWriter
   from PIL import Image
-  # from joblib import Parallel, delayed
-  # import joblib
-  # from tqdm import tqdm  
-  # #ClearMap path
-  # import sys
-  # sys.path.append('/home/user/anaconda3/clearmap/ClearMap2')
-  # #%% Initialize workspace
-  
-  # import pyqtgraph as pg
-  # print(pg.QtCore.PYQT_VERSION_STR)
-  
-  # from ClearMap.Environment import *  #analysis:ignore
-  
-  # #directories and files
-  # directory = '/scratch/201205_LSstandardMouse/'
-  
-  # expression_raw      = 'anno_upscaled_to_rot-stitched.npy'   
-  # expression_auto     = '5657_C1-stitched_AF_lv4.npy'  
-  
-  # ws = wsp.Workspace('CellMap', directory=directory);
-  # ws.update(raw=expression_raw, autofluorescence=expression_auto)  
-  # ws.info()
-  
-  # ws.debug = False
-  
-  # resources_directory = settings.resources_path
-  
-  # #%%Convert tif files to npy files NECCESSARY!
-  # io.convert_files(ws.filename('raw', extension='tif'), extension='npy',
-  #                 processes=40, verbose=True);
-  
-  # #%% Convert npy to tif to load in ImageJ
-  # io.convert_files(ws.filename('raw', extension='npy'), extension='tif',
-  #                 processes=40, verbose=True);
+  import os
+
+  def cls():
+    os.system('cls' if os.name=='nt' else 'clear')
+
   
   #%%############################################################################
-  ### upscale atlas for full-resolution registration 
+  ### Initialization - set parameters
   ###############################################################################
-  ### check image
-  # import cv2
-  # imS = cv2.resize(source_mm[0], (800, 600))                # Resize image
-  # cv2.imshow("output", imS) 
+
   directory = '/scratch/201205_LSstandardMouse/'
-  slice_tmp_folder    = "/scratch/201205_LSstandardMouse/slice_tmp_folder/"
-  resample_tmp_folder = "/scratch/201205_LSstandardMouse/resample_tmp_folder/"
-  x, y, z = 5735, 7578, 2581 #matching resolution
-  #x, y, z = 466*2, 616*2, 259*2 #double the resolution
+  anno_file = 'bspline_annotation_result.1.tif'
   
-  # load the source registration file as mm (write protected) [z,y,x]
-  source_file = os.path.join(directory, "bspline_annotation_result.1.tif")
+  # define the source resolution out of the source annotation file
+  source_file = os.path.join(directory, anno_file)
   source_mm = tifffile.imread(source_file)
+  src_x, src_y, src_z = source_mm.shape[2], \
+                        source_mm.shape[1], \
+                        source_mm.shape[0]
   
-  ### 1. STEP: upscale x,y direction and hold z stable
+  # define the resolution for the target file either manuel or 
+  # match a target file so the annotation can be used as overlay                        
+  target_shape_file = ""
+  
+  if len(target_shape_file) != 0: 
+    memmap_image        = tifffile.memmap(os.path.join(directory, target_shape_file))
+    tar_x, tar_y, tar_z = memmap_image.shape[2], memmap_image.shape[1], memmap_image.shape[0]
+    del memmap_image, target_shape_file
+  else:
+    tar_x, tar_y, tar_z = src_x*2, src_y*2, src_z*2 #matching resolution
+    
+  ###############################################################################
+  # ##  ### 1. STEP: upscale x,y direction and hold z stable
+  ###############################################################################
+  cls()
   starttime = datetime.datetime.now()
   # create the final upscale file (just reserves the space on HDD)
   target_file = os.path.join(directory, "anno_upscaled_to_stitched.npy")
-  target_mm = np.memmap(target_file, dtype="float32", mode="w+", shape=(259,y,x))
+  target_mm = np.memmap(target_file, dtype="float32", mode="w+", shape=(259,tar_y,tar_x))
   
   # loop over all slices:
+  looptime = 0
   for i in range(source_mm.shape[0]):
-    looptime = datetime.datetime.now()
+    looptime += datetime.datetime.now()
   
-    slice = np.array(Image.fromarray(source_mm[i]).resize((x, y), Image.NEAREST))
+    slice = np.array(Image.fromarray(source_mm[i]).resize((tar_x, tar_y), Image.NEAREST))
     target_mm[i] = slice
     target_mm.flush()
-    
-    print(f"slice {i} of {len(source_mm[:,:,0])} in {datetime.datetime.now()-looptime}")
+    if i!=0 and (round(i/source_mm.shape[0],2)*100)%10 == 0: 
+      print(f"slice {i} of {len(source_mm[:,:,0])} ({round(i*100/source_mm.shape[0],1)}%) in {datetime.datetime.now()-looptime}")
   
-  # final print  
-  print(f"{len(source_mm[:,:,0])} slices upscaled in {datetime.datetime.now()-starttime}")
-  # test time = 0:05:18
-  
-  # import pytiff
-  # with pytiff.Tiff(directory + "anno_upscaled_to_stitched.tif", "w", bigtiff=True) as handle:
-  #   handle.write(target_mm)
-  
+  print("##########################################")
+  print(f"S1 x,y upscale done: {len(source_mm[:,:,0])} slices upscaled in {datetime.datetime.now()-starttime}")
+  print("##########################################")
+
   ###############################################################################
   # ##  ### 2. STEP: upscale z direction and hold xy stable
   ###############################################################################
   # # load the source registration file as mm (write protected) [z,y,x]
   source_file = os.path.join(directory, "anno_upscaled_to_stitched.npy")
-  source_mm = np.memmap(source_file, dtype="float32", mode="r", shape=(259,y,x))
+  source_mm = np.memmap(source_file, dtype="float32", mode="r", shape=(src_z,tar_y,tar_x))
   
   # create the final upscale file (just reserves the space on HDD)
-  # target_file = os.path.join(directory, "anno_upscaled_to_xyz-stitched.npy")
-  # target_3d = np.memmap(target_file, dtype="float32", mode="w+", shape=(z,y,x))
-  
   target_file = os.path.join(directory, "anno_upscaled_to_rot-stitched.npy")
-  target_rot = np.memmap(target_file, dtype="float32", mode="w+", shape=(y,z,x))
+  target_rot = np.memmap(target_file, dtype="float32", mode="w+", shape=(tar_y,tar_z,tar_x))
   
   source_mm = np.rot90(source_mm, 1, axes=(0,1))
-  # target_mm = np.rot90(target_mm, 1, axes=(1,2))
   
-  starttime = datetime.datetime.now()
- 
+  starttime2 = datetime.datetime.now()
+  
+  looptime = 0
   # loop over all slices:
   for i in range(source_mm.shape[0]):
-    looptime = datetime.datetime.now()
+    looptime += datetime.datetime.now()
     
-    slice = np.array(Image.fromarray(source_mm[i]).resize((x,z), Image.NEAREST))
-    #slice = np.array(Image.fromarray(source_mm[i]).resize((z, x), Image.NEAREST))
-    #slice = tifffile.imread(f"{resample_tmp_folder}slice_xy-upscaled_{i}.tif")
+    slice = np.array(Image.fromarray(source_mm[i]).resize((tar_x,tar_z), Image.NEAREST))
     target_rot[i] = slice
     target_rot.flush()
     
-    print(f"slice {i} of {len(source_mm[:,:,0])} in {datetime.datetime.now()-looptime}")
+    if i!=0 and (round(i*100/source_mm.shape[0]))%20 == 0: 
+     print(f"slice {i} of {len(source_mm[:,:,0])} ({round(i*100/source_mm.shape[0],1)}%) in {datetime.datetime.now()-looptime}")
   
   target_rot = np.rot90(target_rot, -1, axes=(0,1))
   target_rot.flush()
   
-  # final print  
-  print(f"{len(source_mm[:,:,0])} slices upscaled in {datetime.datetime.now()-starttime}")
-  
+  print("##########################################")
+  print(f"{len(source_mm[:,:,0])} slices upscaled in {datetime.datetime.now()-starttime2}")
+  print("##########################################")
   ###############################################################################
   # ##  ### 3. STEP: convert to tif
   ###############################################################################
-  tifffile.imsave(directory + 'my_image.tif', target_rot)
-  ###############################################################################
-  # ## sanity check!
-  ###############################################################################
   
-  # for i in range(target_rot.shape[0]):
-  #   looptime = datetime.datetime.now()
-  #   slice_save(i)
-  #   print(f"slice {i} of {target_rot.shape[0]} in {datetime.datetime.now()-looptime}")
- 
-  # def slice_save(i):
-  #  #looptime = datetime.datetime.now()
-  #  tifffile.imwrite(f"{slice_tmp_folder}slice_{i}.tif", target_rot[i])
-  #  #print(f"slice {i} of {target_mm.shape[0]} in {datetime.datetime.now()-looptime}")
- 
- # # convert 3d smoothing parallel processing  
-  # with tqdm_joblib(tqdm(desc="My calculation", total=target_rot.shape[0])) as progress_bar:
-  #  results = Parallel(n_jobs=72)(delayed(slice_save)(i) for i in range(target_rot.shape[0])) 
- 
-  # ### check image
-  # import cv2
-  # imS = cv2.resize(target_rot[200], (800, 600))                # Resize image
-  # cv2.imshow("output", imS) 
- 
+
+  # writing
+  with TiffWriter(os.path.join(directory, "bspline_annotation_upscaled_v3.tif"), bigtiff=True) as tif:
+      looptime = 0
+      for i in range(target_rot.shape[0]):
+        looptime += datetime.datetime.now()  
+        tif.save(target_rot[i], photometric='minisblack') # min-is-black
+        if i!=0 and (round(i*100/source_mm.shape[0]))%20 == 0: 
+          print(f"slice {i} of {target_rot.shape[0]} ({round(i*100/target_rot.shape[0],1)}%) in {datetime.datetime.now()-looptime}")
+  
+  ###############################################################################
+  # ##  ### 4. STEP: delete the working files
+  ###############################################################################
+  os.remove(target_file)
+  os.remove(source_file)
+  
+  print("##########################################")
+  print(f"Full processing time: {datetime.datetime.now()-starttime}")
+  print("##########################################")
